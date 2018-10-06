e40e58dcb97bc3177bc66984a0146fa08cd5becb
Laitia Matagavui <lcavukali@yahoo.com>
2018-10-07 00:29:18 +0100

    password reset

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,43 @@
 @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+@section("head")
+  <title>Calibration | Reset Password</title>
+@endsection
+@section('loginContent')
+<div class="login-box">
+        <div class="login-logo">
+          <a href=""><b>Calibration</b></a>
         </div>
-    </div>
-</div>
+        <!-- /.login-logo -->
+        <div class="login-box-body">
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+          <p class="login-box-msg">Forgot Your Password?</p>
+      
+          <form method="POST" action="{{ route('password.email') }}">
+              @csrf
+            <div class="form-group has-feedback">
+                  <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
+              <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+              @if ($errors->has('email'))
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $errors->first('email') }}</strong>
+                  </span>
+              @endif
+            </div>
+            <div class="row">
+                    <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Send Password Reset Link') }}
+                            </button>
+                        </div>
+              <!-- /.col -->
+            </div>
+          </form>
+        </div>
+        <!-- /.login-box-body -->
+      </div>
+      <!-- /.login-box -->
 @endsection
